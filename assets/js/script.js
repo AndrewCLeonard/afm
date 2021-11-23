ee36d20dcb1658c75b4595b8bd7228ed774a840b
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-23 09:51:58 -0800

    basic html setup

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var selectors = {};
+var classes = {};
+var data = {};
+
+// dom references
+var queryInputEl = document.querySelector("#query-input");
+var querySearchTerm = document.querySelector("#query-search-term");
+
+// Event Listeners
