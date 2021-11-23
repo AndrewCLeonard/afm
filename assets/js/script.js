9422142073a81a22da92f8aca46e28189b8d6fe5
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-23 13:22:00 -0800

    adding api fetch functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,32 @@ var selectors = {};
 var classes = {};
 var data = {};
 
-// dom references
+// DOM REFERENCES START VVV----------------------------------------------------------------VVV
+// queryInputEl is the text input
 var queryInputEl = document.querySelector("#query-input");
-var querySearchTerm = document.querySelector("#query-search-term");
+// queryFormEl is the entire form element
+var queryFormEl = document.querySelector("#query-form");
+// for manipulating any eventual data labels on buttons
+var querySearchOption = document.querySelector("#query-search-option");
+// DOM REFERENCES END ^^^----------------------------------------------------------------^^^
 
-// Event Listeners
+// FUNCTION SECTION START VVV----------------------------------------------------------------VVV
+
+var getMovieInfo = function (query) {
+	// format the api url
+	var apiUrl = "https://imdb-api.com/en/API/SearchMovie/k_vm8gj4hz/" + query;
+};
+
+// Captures form data and sends it to _____ function for fetch request.     N.B. e = "event"
+var formSubmitHandler = function (e) {
+	// prevent page refresh
+	e.preventDefault();
+
+	// get value from input element
+	var query = queryInputEl.value.trim();
+};
+// FUNCTION SECTION END ^^^----------------------------------------------------------------^^^
+
+// EVENT LISTENERS START: event listeners for form & button container VVV----------------------------------------------------------------VVV
+queryFormEl.addEventListener("submit", formSubmitHandler);
+// EVENT LISTENERS END ^^^----------------------------------------------------------------^^^
