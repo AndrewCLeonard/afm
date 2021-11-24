a104658c04c4e55ed16c919162b30806cd0e6644
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-23 16:07:56 -0800

    working on js movie search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,55 @@ var data = {};
 var queryInputEl = document.querySelector("#query-input");
 // queryFormEl is the entire form element
 var queryFormEl = document.querySelector("#query-form");
+// selector to create div where movie search results will be displayed
+var movieContainerEl = document.querySelector("#movies-container");
+// selector for movie search term
+var movieSearchTerm = document.querySelector("#movie-search-term");
 // for manipulating any eventual data labels on buttons
 var querySearchOption = document.querySelector("#query-search-option");
 // DOM REFERENCES END ^^^----------------------------------------------------------------^^^
 
 // FUNCTION SECTION START VVV----------------------------------------------------------------VVV
 
-var getMovieInfo = function (query) {
+//use API `fetch` request to get movie titles w/ related data as JSON
+var getMovieInfo = function (movieSearchTerm) {
 	// format the api url
-	var apiUrl = "https://imdb-api.com/en/API/SearchMovie/k_vm8gj4hz/" + query;
+	var apiUrl = "https://imdb-api.com/en/API/SearchMovie/k_vm8gj4hz/" + movieSearchTerm;
+
+	// make a `get` request to url
+	fetch(apiUrl).then(function (response) {
+		// request was successful
+		if (response.ok) {
+			response.json().then(function (data) {
+				console.log("data = ", data);
+				displayMovies(data, user);
+			});
+		} else {
+			alert("Unable to connect to Open Movie Database"); // ??? Not allowed to use `alerts` in this project?
+		}
+	});
+};
+
+// Create DOM eleents from the JSON data acquired in `getMovieInfo` function
+var displayMovies = function (movies, searchTerm) {
+	// check if api search returned any results
+	if (movies.length === 0) {
+		movieContainerEl.textContent = "No movies found";
+		return;
+	}
+
+	movieSearchTerm.textContent = searchTerm;
+
+	// loop over api search results
+	for (var i = 0; i < movies.length; i++) {
+		// format how movie names are displayed as "TITLE (RELEASE YEAR)"
+		var movieTitle = movies[i].fullTitle;
+
+		// create container for each movie result
+		var movieEl = document.createElement("a");
+		movieEl.classList = ""; /* ??? Add classes to turn each movie into a card */
+		movieEl.setAttribute("href", "")
+	}
 };
 
 // Captures form data and sends it to _____ function for fetch request.     N.B. e = "event"
